858b2686537974b79a92031edda410d1cbc643d0
sketchsnipe <sketchsnipe@protonmail.com>
2021-05-17 21:43:11 -0400

    init

---
+++ README.txt
@@ -0,0 +1,10 @@
+NAME
+	late - Live Appending Text Editor
+
+DESCRIPTION
+	`late` is a simple, extensable text stream viewer and editor. It will print the live appends
+	to a file similar to `tail -f` and allow you to `type stuff [enter]' and append to the file.
+		This program is designed to be used with 
+
+SYNOPSIS
+	late [read file] [write file]
---
+++ late.c
@@ -0,0 +1,17 @@
+/* late.c -- live appending text editor
+
+	The person who associated a work with this deed has dedicated the work to
+	the public domain by waiving all of his or her rights to the work worldwide
+	under copyright law, including all related and neighboring rights, to the
+	extent allowed by law. Do whatever you want. You can copy, modify, distribute
+	and perform this work, even for commercial purposes, all without asking
+	permission.
+
+	This software is provided `as is`, and in fact, if you copy this code consider
+	yourself liable for running as if you wrote it.
+*/
+
+#include <stdio.h>
+#include <stdlib.h> // for exit()
+
+
